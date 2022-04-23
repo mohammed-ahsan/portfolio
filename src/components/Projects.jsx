c8a9b8f82dd8504db0842838deb2449171570c68
Ahsan <37025966+mohammed-ahsan@users.noreply.github.com>
2022-04-24 02:09:15 +0600

    updated shadow on project goals

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -228,7 +228,7 @@ const Projects = () => {
                 <SwiperSlide>
                   <div className="flex flex-col">
                     <img name="slide" className="rounded-b-lg" src={Project3} alt="Project_image" />
-                    <div className="flex md:flex-row gap-1 flex-col  md:w-[50%] h-[50%] p-2 border-2 bg-gradient-to-br from-[#8b8574] to-[#FFEAD7] rounded-xl m-auto">
+                    <div className="flex md:flex-row gap-1 flex-col  md:w-[50%] h-[58%] p-2 border-2 bg-gradient-to-b2 from-[#8b8574] to-[#FFEAD7] rounded-xl m-auto">
                       <div className=" md:w-[50%] ">
                         
                         <p className="border-b-2 text-md md:w-auto w-fit mx-auto font-semibold text-gray-700">
@@ -251,11 +251,11 @@ const Projects = () => {
         {/*project-goals*/}
         
         <div className="parent w-[70%] mx-auto grid grid-cols-6 gap-4 mb-[100px]">
-  <div className="child-1 rounded-t-lg duration-300 col-start-2 col-span-4 bg-gray-700 hover:bg-gray-900 p-3 border-2 border-[#8b8574] "><div className="text-[#e5dcc2] font-medium w-fit mx-auto">Clean & Optimized Code</div></div>
-  <div className="child-2 rounded-l-lg duration-300 col-start-1 col-end-3 bg-gray-700 hover:bg-gray-900 p-3 border-2 border-[#8b8574]"><div className="text-[#e5dcc2] font-medium w-fit mx-auto">Lesser Bugs</div></div>
+  <div className="child-1 rounded-t-lg shadow-md shadow-[#8b8574] duration-300 col-start-2 col-span-4 bg-gray-800 hover:bg-gray-900 p-3 border-2 border-[#8b8574] "><div className="text-[#e5dcc2] font-medium w-fit mx-auto">Clean & Optimized Code</div></div>
+  <div className="child-2 rounded-l-lg shadow-md shadow-[#8b8574] duration-300 col-start-1 col-end-3 bg-gray-800 hover:bg-gray-900 p-3 border-2 border-[#8b8574]"><div className="text-[#e5dcc2] font-medium w-fit mx-auto">Lesser Bugs</div></div>
 <div className="m-auto col-start-3 col-span-2"> <div className="font-semibold text-[#e5dcc2] text-xl border-b-2 border-gray-500 ">PROJECT GOALS </div></div>
-  <div className="child-3 rounded-r-lg  duration-300 col-end-7 col-span-2 bg-gray-700 hover:bg-gray-900 p-3 border-2 border-[#8b8574]"><div className="text-[#e5dcc2] font-medium w-fit mx-auto">Better SEO</div></div>
-  <div className="child-4 rounded-b-lg   duration-300 col-start-1 col-end-7 bg-gray-700 hover:bg-gray-900 p-3 border-2 border-[#8b8574]"><div className="text-[#e5dcc2] font-medium w-fit mx-auto">High Performance & ReUsable</div></div>
+  <div className="child-3 rounded-r-lg shadow-md shadow-[#8b8574] duration-300 col-end-7 col-span-2 bg-gray-800 hover:bg-gray-900 p-3 border-2 border-[#8b8574]"><div className="text-[#e5dcc2] font-medium w-fit mx-auto">Better SEO</div></div>
+  <div className="child-4 rounded-b-lg shadow-md shadow-[#8b8574] duration-300 col-start-1 col-end-7 bg-gray-800 hover:bg-gray-900 p-3 border-2 border-[#8b8574]"><div className="text-[#e5dcc2] font-medium w-fit mx-auto">High Performance & ReUsable</div></div>
 </div>
         
       </div>
